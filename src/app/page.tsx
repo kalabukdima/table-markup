30c746ce59ed274d3b1bfced97f99e279f9b2082
Kalabuk Dzmitry <kalabukdima@gmail.com>
2023-08-09 13:28:32 +0200

    Implement editing multiple tables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import { Property as CssProperties } from "csstype";
 import {
   Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, AppBar, Box, Toolbar, Typography,
   IconButton, Pagination, ButtonGroup, Button, Radio, FormGroup, PaginationItem, useMediaQuery, TextField, RadioGroup,
-  FormControlLabel
+  FormControlLabel,
+  useTheme
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2"
 import { createTheme } from "@mui/material";
@@ -17,7 +18,7 @@ import AppsIcon from "@mui/icons-material/Apps";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
 import { Point, fillRectangle } from "@/lib/matrix";
-import { FilledTableData, samples, CellType } from "@/lib/data";
+import { FilledTableData, samples, CellType, TableType } from "@/lib/data";
 
 declare module '@mui/material/Button' {
   interface ButtonPropsVariantOverrides {
@@ -28,7 +29,7 @@ declare module '@mui/material/Button' {
 const theme = createTheme();
 
 function cellColor(type: CellType): CssProperties.Color {
-  if (type === undefined) {
+  if (!type) {
     return "default";
   }
   return {
@@ -43,13 +44,18 @@ function Spacer() {
   return <Box margin="auto" />;
 }
 
-function Header({ count, page, setPage }: { count: number, page: number, setPage: (x: number) => void }) {
+type PageStatus = {
+  done: boolean;
+}
+
+function Header(props: {
+  pages: PageStatus[],
+  currentPage: number,
+  setPage: (x: number) => void,
+}) {
+  const theme = useTheme();
   const small = useMediaQuery(theme.breakpoints.down("sm"), { defaultMatches: true });
   const medium = useMediaQuery(theme.breakpoints.down("md"));
-
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-  };
 
   return (
     <AppBar position="static">
@@ -64,8 +70,8 @@ function Header({ count, page, setPage }: { count: number, page: number, setPage
           <HomeIcon />
         </IconButton>
         <Pagination
-          page={page} count={count}
-          onChange={handlePageChange}
+          page={props.currentPage} count={props.pages.length}
+          onChange={(_event, value) => props.setPage(value)}
           siblingCount={small ? 1 : medium ? 2 : 6} boundaryCount={1} shape="rounded"
           sx={{
             // For some reason the PaginationItem style below doesn't affect ellipsis
@@ -77,7 +83,7 @@ function Header({ count, page, setPage }: { count: number, page: number, setPage
           }}
           renderItem={item => (
             <PaginationItem {...item} sx={{
-              color: item.page === 5 && item.type === "page" ? "darkgray" : "inherit"
+              color: item.type === "page" && item.page && (props.pages[item.page - 1]?.done ?? false) ? theme.palette.text.disabled : "inherit"
             }} />
           )}
         />
@@ -88,8 +94,10 @@ function Header({ count, page, setPage }: { count: number, page: number, setPage
   )
 }
 
-function TableViewer(props: Pick<FilledTableData, "table_array" | "annotations"> & {
-  paintType: CellType,
+function TableViewer(props: {
+  tableData: FilledTableData,
+  setAnnotations: (annotations: CellType[][]) => void,
+  paintType: string,
 }) {
   const [selection, setSelection] = React.useState<{
     start: Point | undefined,
@@ -99,7 +107,14 @@ function TableViewer(props: Pick<FilledTableData, "table_array" | "annotations">
     end: undefined
   })
 
-  const [annotations, setAnnotations] = React.useState(props.annotations);
+  const annotations = props.tableData.annotations;
+  const paintType = {
+    "header": "header",
+    "attributes": "attributes",
+    "data": "data",
+    "metadata": "metadata",
+    "clear": undefined,
+  }[props.paintType] as CellType;
 
   const setSelectionStart = (x: number, y: number) => {
     setSelection({
@@ -118,13 +133,13 @@ function TableViewer(props: Pick<FilledTableData, "table_array" | "annotations">
   }
   const confirmSelection = () => {
     if (selection.start && selection.end) {
-      setAnnotations(fillRectangle(annotations, [selection.start, selection.end], props.paintType));
+      props.setAnnotations(fillRectangle(annotations, [selection.start, selection.end], paintType));
     }
     clearSelection();
   };
 
   const visibleAnnotations = (!selection.start || !selection.end) ? annotations : fillRectangle(
-    annotations, [selection.start, selection.end], props.paintType
+    annotations, [selection.start, selection.end], paintType
   );
 
   return (
@@ -134,7 +149,7 @@ function TableViewer(props: Pick<FilledTableData, "table_array" | "annotations">
     >
       <Table>
         <TableBody>
-          {props.table_array.map((row, row_index) => (
+          {props.tableData.table_array.map((row, row_index) => (
             <TableRow
               key={row_index}
               sx={{
@@ -184,40 +199,92 @@ function TableColumnsIcon() {
   return <TableRowsIcon sx={{ rotate: "90deg" }} />
 }
 
+type Dataset = {
+  tables: FilledTableData[];
+}
+
+function isMarkedUp(table: FilledTableData): boolean {
+  if (table.header_type != "other") {
+    return true;
+  }
+  if (!table.annotations) {
+    return false;
+  }
+  return table.annotations.find(row => (row.find(x => x) != undefined)) != undefined
+}
+
 export default function DataView() {
+  const [dataset, setDataset] = React.useState<Dataset>({ tables: samples });
   const [page, setPage] = React.useState(1);
-  const [paintType, setPaintType] = React.useState<CellType>("header");
+  const tableIndex = page - 1;
+  const table = dataset.tables[tableIndex];
+  if (table.header_type == undefined) {
+    table.header_type = "other";
+  }
+  const [paintType, setPaintType] = React.useState("header");
+
+  const setTableData = (newData: FilledTableData) => {
+    const newTables = dataset.tables.map((data, index) => index == tableIndex ? newData : data);
+    setDataset({
+      tables: newTables,
+    });
+  };
+  const setAnnotations = (newValue: CellType[][]) => {
+    setTableData({
+      ...table,
+      annotations: newValue,
+    });
+  };
+  const setHeaderType = (newValue: string) => {
+    setTableData({
+      ...table,
+      header_type: newValue as TableType,
+    })
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <Header count={samples.length} page={page} setPage={setPage} />
+      <Header pages={dataset.tables.map(table => ({ done: isMarkedUp(table) }))} currentPage={page} setPage={setPage} />
       <Box padding={4}>
         <FormGroup>
           <Grid container spacing={2} alignItems={"stretch"} width={1}>
             <Grid width="100%">
               <TableViewer
-                {...samples[0]}
+                key={tableIndex}
+                tableData={table}
+                setAnnotations={setAnnotations}
                 paintType={paintType}
               />
             </Grid>
             <Grid>
               <RadioGroup
-                name="controlled-radio-buttons-group"
                 value={paintType}
-                onChange={e => setPaintType(e.target.value as CellType)}
+                onChange={e => setPaintType(e.target.value)}
               >
                 <FormControlLabel value="header" control={<Radio />} label="Header" />
                 <FormControlLabel value="attributes" control={<Radio />} label="Attributes" />
                 <FormControlLabel value="data" control={<Radio />} label="Data" />
                 <FormControlLabel value="metadata" control={<Radio />} label="Metadata" />
+                <FormControlLabel value="clear" control={<Radio />} label="Clear" />
               </RadioGroup>
             </Grid>
             <Grid>
-              <ButtonGroup variant="contained">
+              <RadioGroup
+                value={table.header_type}
+                onChange={e => setHeaderType(e.target.value)}
+              >
+                <FormControlLabel value="horizontal" control={<Radio/>} label={<TableRowsIcon />} />
+                <FormControlLabel value="vertical" control={<Radio/>} label={<TableColumnsIcon />} />
+                <FormControlLabel value="matrix" control={<Radio/>} label={<AppsIcon />} />
+                <FormControlLabel value="trash" control={<Radio/>} label={<QuestionMarkIcon />} />
+                <FormControlLabel value="other" control={<Radio/>} label="Unset" />
+              </RadioGroup>
+              {/* <ButtonGroup variant="contained">
                 <IconButton size="large" color="primary"><TableRowsIcon /></IconButton>
                 <IconButton size="large"><TableColumnsIcon /></IconButton>
                 <IconButton size="large"><AppsIcon /></IconButton>
                 <IconButton size="large"><QuestionMarkIcon /></IconButton>
-              </ButtonGroup>
+              </ButtonGroup> */}
             </Grid>
           </Grid>
         </FormGroup>
